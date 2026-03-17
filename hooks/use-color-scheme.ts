b9730b5411fd8c59c01b76c7e9030d38b1b41707
coolport <aidanalcayde@gmail.com>
2026-03-17 18:54:48 +0800

    feat: add useColorScheme hook

--- hooks/use-color-scheme.ts
+++ hooks/use-color-scheme.ts
@@ -1,1 +1,6 @@
-export { useColorScheme } from 'react-native';
+import { usePlantStore } from '@/store/usePlantStore';
+
+export function useColorScheme() {
+  const settings = usePlantStore((state) => state.settings);
+  return settings.darkMode ? 'dark' : 'light';
+}
